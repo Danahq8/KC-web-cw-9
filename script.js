97d947f1e30661591ccf032a89eaa6c047b461ae
Danahq8 <alomrandanah@gmail.com>
2022-11-05 19:33:32 +0300

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
+let orders=[];
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +8,26 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
+  let addOrder=[
+    {name,id,phone,order},
+  ]
+  
+
   
   // أدرج الكائن إلى المصفوفة
+   addOrder.push(addOrder)
   
   // طبق الخطوة 7 تحت هذا الكومنت
+  load()
   
 }
 
-function load() {
+function load(addOrder) {
   let container = document.getElementById("container");
   container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
+orders.forEach(item => {
+     container.innerHTML+= `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`
 
+});
 }
